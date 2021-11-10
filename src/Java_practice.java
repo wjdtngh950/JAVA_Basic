b809cebca8c1a43cc664a9d497579981e5470dc9
wjdtngh950 <wjdtngh950@naver.com>
2021-11-10 17:57:00 +0900

    Update Java_practice.java

--- src/Java_practice.java
+++ src/Java_practice.java
@@ -1,6 +1,6 @@
 public class Java_practice {
     public static void main(String[] args) {
-        System.out.println("my name is suhos");
+        System.out.println("my name is sdfdfsos");
     }
 }
 
