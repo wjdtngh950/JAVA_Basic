b50c50268d16083246a4894a9cbfb110f2b5cc88
wjdtngh950 <wjdtngh950@naver.com>
2021-12-29 17:54:42 +0900

    Update Java_practice.java

--- src/Java_practice.java
+++ src/Java_practice.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 public class Java_practice {
     public static void main(String[] args) {
         int va1 = 11;
         long va2 = 11L;
         double val3 = 11.0;
         char val4 = 'c';
-        String str = "i am string";
         boolean val5 = true;
         boolean val6 = false;  //python -> Ture, False
+        var tmp = 99;
+        //String Class
+        String str = "i am string";
 
         //Wrapper Class
         Integer val7 = 11;
@@ -15,6 +19,12 @@ public class Java_practice {
         Character val10 = 'c';
         Boolean val11 = Boolean.TRUE;
         Boolean val12 = Boolean.FALSE;
+
+        //Collection = C++ STL
+        //List 자료구조: 배열기반 리스트, 연결리스트
+        ArrayList<>
+
+
     }
 }
 
